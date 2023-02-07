7d71e0610d075838740c2c6771f154604b1f8b0e
saba0308 <sabapathi030820@gmail.com>
2023-02-07 16:51:57 +0530

    2.0.1

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json'); // <== Will be created later
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3200; // <== You can change the port
 
-server.use(middlewares);
-server.use(router);
 
-server.listen(port);
+server.use(middlewares)
+// Add this before server.use(router)
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id'
+}))
+server.use(router)
+server.listen(3000, () => {
+    console.log('JSON Server is running')
+})
 
 // Export the Server API
 module.exports = server
